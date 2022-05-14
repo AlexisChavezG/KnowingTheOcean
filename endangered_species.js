d6982aa56e7e7ade9d5fac6e903b5349d00492a4
jesusbeb <jesusbelb@gmail.com>
2022-05-14 12:37:08 -0500

    Primera version del archivo que contiene especies marinas en peligro de extinción

---
+++ endangered_species.js
@@ -0,0 +1,100 @@
+[
+    {
+      "name": "Totoaba",
+      "nombreCientifico": "Totoaba macdonaldi",
+      "id": 1,
+      "familia": "Sciaenidae",
+      "habitat": "Golfo de California", 
+      "otros_nombres": [
+        "corvina",
+        "cabicucho",
+      ]
+    },
+    {
+      "name": "Tortuga carey",
+      "nombreCientifico": "Eretmochelys Imbricata",
+      "id": 2,
+      "familia": "quelónidos",
+      "otros_nombres": [
+        "hawksbill sea turtle",
+        "pico de halcon"
+      ]
+    },
+    {
+      "name": "Vaquita Marina",
+      "nombreCientifico": "Phocoena Sinus",
+      "id": 3,
+      "familia": "Phocoenidae",
+      "otros_nombres": [
+        "cochito",
+        "Marsopa vaquita",
+        "Vaquita"
+      ]
+    },
+    {
+      "name": "Ballena Azul",
+      "nombreCientifico": "Balaenoptera",
+      "id": 4,
+      "familia": "Balaenopteridae",
+      "otros_nombres": [
+        "rorcual azul",
+        "Ballena con barbas"
+      ]
+    },
+    {
+      "name": "Tortuga lora",
+      "nombreCientifico": "Lepidochelys kempii",
+      "id": 5,
+      "familia": "Cheloniidae",
+      "otros_nombres": [
+        "tortuga cotorra",
+        "tortuga bastarda",
+      ]
+    },
+    {
+      "name": "León Marino de Steller",
+      "nombreCientifico": "Eumetopias jubatus",
+      "id": 6,
+      "familia": "otáridos",
+      "otors_nombres": [
+        "León marino del norte"
+      ]
+    },
+    {
+      "name": "Tiburon Martillo Gigante",
+      "nombreCientifico": "Sphyrna mokarran",
+      "id": 7,
+      "familia": "Sphyrnidae",
+      "otros_nombres": [
+          "Tiburon cornuda gigante"
+      ]
+    },
+    {
+      "name": "Rorcual común",
+      "nombreCientifico": "Balaenoptera physalus",
+      "score": 8,
+      "familia": "Balaenopteridae",
+      "otros_nombres": [
+        "ballena de aleta"
+      ]
+    },
+    {
+      "name": "Delfin de Cabeza Blanca",
+      "nombreCientifico": "Cephalorhynchus",
+      "id": 9,
+      "familia": "Delphinidae",
+      "stacks": [
+        "delfín de Héctor",
+        "delfín cara blanca"
+      ]
+    },
+    {
+      "name": "Tortuga Verde",
+      "nombreCientifico": "Chelonia mydas",
+      "id": 10,
+      "familia": "Cheloniidae",
+      "otros_nombres": [
+        "honu"
+      ]
+    }
+    ] 
