e96d620dd317e63fcfda22b56ed8b9d297cb125a
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-15 12:44:22 -0500

    Refactoring prueba de unidad de read.js

--- test/utils/read.test.js
+++ test/utils/read.test.js
@@ -2,7 +2,7 @@ const Reader = require("../../libs/utils/read");
 
 describe("Test for Reader", () => {
     test("Create a Reader object and verify that it isn't empty", () => {
-        const explorers = Reader.readJsonFile();
-        expect(explorers.length).not.toBe(0);
+        const animals = Reader.readJsonFile();
+        expect(animals.length).not.toBe(0);
     });
 });
